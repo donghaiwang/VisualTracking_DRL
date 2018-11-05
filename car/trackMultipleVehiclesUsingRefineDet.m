7d62a662e7ffbbe900349034e4ec01981471f1fe
EricWu123 <630257405@qq.com>
2018-11-05 21:52:43 +0800

    display wrong result

--- car/trackMultipleVehiclesUsingRefineDet.m
+++ car/trackMultipleVehiclesUsingRefineDet.m
@@ -20,14 +20,22 @@ end
 refineDet = py.importlib.import_module('RefineDet');
 refineDetModel = refineDet.init();
 
-image_name_1 = '/home/dong/rl/VisualTracking_DRL/car/tmp/2.jpg';
-% test_image_1 = imread(image_name_1);
+image_name_1 = '/home/laoli/rl/VisualTracking_DRL/car/tmp/1.jpg';
+test_image_1 = imread(image_name_1);
 % Conversion of MATLAB 'uint8' to Python is only supported for 1-N vectors.
 detectRes1 = refineDetModel.detection(py.str(image_name_1));
+
 % detectRes1{end}
 for i = 1 : length(detectRes1)
-    disp(detectRes1{i});
+    boxList = detectRes1{i};
+    x1 = double(boxList{1});
+    y1 = double(boxList{2});
+    x2 = double(boxList{3});
+    y2 = double(boxList{4});
+    test_image_1 = insertObjectAnnotation(test_image_1, 'rectangle', [x1 y1 x2 y2], '', 'Color', 'yellow', ...
+        'FontSize', 10, 'TextBoxOpacity', .8, 'LineWidth', 2);
 end
+imshow(test_image_1);
 disp(length(detectRes1));
 
 
